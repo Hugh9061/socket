2474fc8a7eb2a55a2e6c27891ce5c01e09378206
Hugh <49238684+Hugh9061@users.noreply.github.com>
2020-03-25 09:03:04 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,51 +1,50 @@
-import socket
-import threading
+from socket import *
+from time import strftime, localtime
+from select import select
+from json import loads
 
-SERVER_ADDRESS = ('192.168.2.100',9999)
+HOST = ''
+POST = 3000
+BUFSIZ = 1024
+ADDR = (HOST, POST)
 
-Server = None
-Server_Pool = []
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+tcpSerSock.setblocking(False)
 
+inputs = [tcpSerSock]
 
-class Server():
+print('waiting for connnecting...')
+
+while True:
     
-    # 初始化
-    def __init__(self,host = '192.168.2.100',port = 9999,listen_count = 5):
-        self.client_pool = []  #客户端线程池
-        self.host = host,
-        self.port = port,
-        self.listen_count = listen_count
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.socket.bind((host,port))
-        self.socket.listen(listen_count)
-        print('Wating for connection......')
-    
-    
-    # 用来处理每一个线程接收的会话
-    def handle_message(self,client,address):   
-        client_name = client.recv(1024).decode('utf-8')  #用户名
-        print('Accect new connection from {}'.format(client_name))
-        client.send('welcome to Server: {}'.format(client_name).encode('utf-8'))
-        while True:
-            recv_message =  client.recv(1024).decode('utf-8') #获取客户端传递的消息
-            print('{} ： '.format(client_name),recv_message)
-            send_message = input('回复{} :   '.format(client_name)).encode('utf-8') #待发送的消息
-            client.send(send_message)
+    rlist, wlist, xlist = select(inputs, inputs, [])
+
+    for s in rlist:
+        
+        if s is tcpSerSock:
+            tcpCliSock, addr = s.accept()
+            print('...connecting from:', addr)
+            tcpCliSock.setblocking(False)
+            inputs.append(tcpCliSock)
             
-        
-    # 开启一个线程用于接收客户端链接
-    def acception(self):    
-        client,address = self.socket.accept()
-        self.client_pool.append((client,address))
-        t = threading.Thread(target=self.handle_message,args=(client,address))
-        t.start()
-        
-        
-        
-if __name__ == "__main__":
-    server = Server()
-    while True:
-        server.acception()
-    
-    
-    
+        else:
+            data = s.recv(BUFSIZ)
+
+            if not data:
+                inputs.remove(s)
+                s.close()
+                continue
+
+            obj = loads(data.decode('utf-8'))
+            time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+            data = '[{}]{}: {}'.format(time, obj['name'], obj['msg'])
+            for sock in inputs:
+                if sock is not tcpSerSock:
+                    sock.send(data.encode('utf-8'))
+
+    for s in wlist:
+
+        if s is tcpSerSock:
+            print('已经写入')
